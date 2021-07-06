df660a7d123463e8d8106a07f1e2697d0a35e24b
buithaihung <buithaihung6934@gmail.com>
2021-07-06 09:35:27 +0700

    add new project

---
+++ 01-starting-project-02/src/components/Users/AddUser.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from  '../UI/Card';
+import classes from './AddUser.module.css';
+import Button from '../UI/Button';
+const AddUser = (props) => {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+
+    }
+
+    return(
+
+    <Card className={classes.input}>
+
+     <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"/>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number"/>
+        <Button type="submit">Add User</Button>
+    </form>
+    </Card>
+    )
+}
+export default AddUser;
